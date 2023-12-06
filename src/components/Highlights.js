47034c07eb3a0a0b60d11e2520427d10f21d5430
joaopeixotogjpm <joaopeixoto@grupojpm.pt>
2023-12-06 09:55:21 +0000

    just footer and owners needed

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,3 +1,7 @@
+import des1 from "../icons_assets/greek salad.jpg"
+import des2 from "../icons_assets/lemon dessert.jpg"
+import des3 from "../icons_assets/bruchetta.svg"
+
 function Highlights(){
     return(
         <section className="Highlights">
@@ -6,35 +10,41 @@ function Highlights(){
                 <button type="button">Menu</button>
             </section>
             <section className="high2">
-                <card>
-                    <img src={'..\\icons_assets\\greek salad.jpg'}/>
+                <card style={{animationDelay:'0.3s'}}>
+                    <img src={des1}/>
                     <section className="SpecialName">
                         <h2>PT salad</h2>
-                        <h4>10.99€</h4>
+                        <h4>3.99€</h4>
                     </section>
+                    <section className="DescriptionCard">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse arcu massa, iaculis et molestie quis, volutpat non dolor. Praesent convallis, nisl ac fermentum finibus,
                          ipsum dolor placerat est, in dapibus risus libero quis quam.</p>
                     <a>Order it -></a>
+                    </section>
                 </card>
                 <card>
-                    <img src=""/>
+                    <img src={des2}/>
                     <section className="SpecialName">
-                        <h2>PT salad</h2>
-                        <h4>10.99€</h4>
+                        <h2>Lemon Des</h2>
+                        <h4>1.99€</h4>
                     </section>
+                    <section className="DescriptionCard">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse arcu massa, iaculis et molestie quis, volutpat non dolor. Praesent convallis, nisl ac fermentum finibus,
                          ipsum dolor placerat est, in dapibus risus libero quis quam.</p>
                     <a>Order it -></a>
+                    </section>
                 </card>
                 <card>
-                    <img src=""/>
+                    <img src={des3}/>
                     <section className="SpecialName">
-                        <h2>PT salad</h2>
-                        <h4>10.99€</h4>
+                        <h2>Bruchetta</h2>
+                        <h4>7.99€</h4>
                     </section>
+                    <section className="DescriptionCard">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse arcu massa, iaculis et molestie quis, volutpat non dolor. Praesent convallis, nisl ac fermentum finibus,
                          ipsum dolor placerat est, in dapibus risus libero quis quam.</p>
                     <a>Order it -></a>
+                    </section>
                 </card>
             </section>
         </section>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,23 +1,30 @@
 function Testimonial(){
 return (
-    <>
-    <h1>Testimonials</h1>
-    <card>
-        <img src=""/>
-        <h4>Nome</h4>
-        <p>Avaliação</p>
-    </card>
-    <card>
-        <img src=""/>
-        <h4>Nome</h4>
-        <p>Avaliação</p>
-    </card>
-    <card>
-        <img src=""/>
-        <h4>Nome</h4>
-        <p>Avaliação</p>
-    </card>
-    </>
+    <section className="Testimonials">
+        <h1>Testimonials</h1>
+        <section className="TestCards">
+            <card>
+                <img src=""/>
+                <h4>João Costa</h4>
+                <p>Avaliação</p>
+            </card>
+            <card>
+                <img src=""/>
+                <h4>Bruna Melo</h4>
+                <p>Avaliação</p>
+            </card>
+            <card>
+                <img src=""/>
+                <h4>Pedro Miguel</h4>
+                <p>Avaliação</p>
+            </card>
+            <card>
+                <img src=""/>
+                <h4>Maria Dias</h4>
+                <p>Avaliação</p>
+            </card>
+        </section>
+    </section>
 )
 }
 export default Testimonial
